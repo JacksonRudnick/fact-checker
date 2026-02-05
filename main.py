e9b6ba49d2999e994255c8da56e29d61db273744
Jackson Rudnick <jacksonrudnick32@gmail.com>
2026-02-04 21:18:57 -0600

    start

---
+++ main.py
@@ -0,0 +1,25 @@
+#load datasets
+
+#grab wikipedia data
+
+#isolate important sentence based on sentence id
+
+#run bert on each sentence in article
+#fine tune as needed
+
+#graph all data using bert output
+#all nodes should be connected
+
+#run graph attention network on graph
+#fine tune as needed
+
+#run each node through a linear layer to get single value
+
+#softmax all node weights 
+
+#multiply each node vector by node weight
+
+#feed into linear layer to get 3 class output
+
+#softmax to get final output probabilities
+#0=supports, 1=refutes, 2=not enough info
